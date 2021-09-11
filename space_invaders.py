f6569caaed64daf174250c493dac488d0a3108f1
teckjuec <teckjue.chua@student.unimelb.edu.au>
2021-09-11 14:20:17 +1000

    Added bullet spawning from player when space is pressed

--- space_invaders.py
+++ space_invaders.py
@@ -12,7 +12,7 @@ class Player(GameEntity):
     playerSizeHeight = 15
     playerSizeWidth = 35
     frames = 0
-
+    shooting = False
 
     def __init__(self, x, y, gameDisplay):
         self.rect = Rect(x-(self.playerSizeWidth/2), y, self.playerSizeWidth, self.playerSizeHeight)
@@ -46,6 +46,7 @@ class Player(GameEntity):
     def printPlayer(self):
         pygame.draw.rect(self.gameDisplay, (255, 0, 0), self.rect)
 
+
     def boundaries(self):
         if self.rect.left < 0:
             self.rect.right = constants.WINDOW_LENGTH
@@ -62,6 +63,12 @@ class Player(GameEntity):
     def getYCentre(self):
         return (self.rect.top + self.rect.bottom)/2
 
+    def shoot(self, bullet_list):
+        bullet = Bullet(self.getXCentre(), self.getYCentre(), self.gameDisplay)
+        bullet_list.append(bullet)
+        
+
+
 
 class Asteroid(GameEntity):
     def __init__(self, x, y):
@@ -73,43 +80,25 @@ class Bullet(GameEntity):
     bulletSizeHeight = 100
     radius = 5
 
-    x = 200
-    y = 300
+    x = 0
+    y = 0
     frames = 0
 
     def __init__(self, x, y, gameDisplay):
         self.rect = pygame.Rect(self.x-self.radius, self.y-self.radius, self.radius*2, self.radius*2) #left, top, width, height
-        self.gameDisplay = gameDisplay   
+        self.gameDisplay = gameDisplay
+        self.x = x
+        self.y = y   
     
     def printBullet(self):
         pygame.draw.circle(self.gameDisplay, (255, 0, 0), (self.x, self.y), self.radius)
 
-    def update(self, pressed_keys):
+    def update(self):
         self.printBullet()
-
-        if pressed_keys[K_RIGHT]:
-            if self.frames == 0:
-                self.frames = 2
-                self.rect.move_ip(1,0)
-                self.x += 1
-
-        if pressed_keys[K_LEFT]:
-            if self.frames == 0:
-                self.frames = 2
-                self.rect.move_ip(-1,0)
-                self.x -= 1 
-
-        if pressed_keys[K_UP]:
-            if self.frames == 0:
-                self.frames = 2
-                self.rect.move_ip(0,-1)
-                self.y -= 1
-
-        if pressed_keys[K_DOWN]:
-            if self.frames == 0:
-                self.frames = 2
-                self.rect.move_ip(0,1)
-                self.y += 1
+        if self.frames == 0:
+            self.frames = 2
+            self.rect.move_ip(0,-1)
+            self.y -= 1
 
         if self.frames > 0:
             self.frames -= 1
@@ -123,11 +112,13 @@ def main():
     
 
     gameDisplay = pygame.display.set_mode([constants.WINDOW_LENGTH, constants.WINDOW_HEIGHT])
-
-    player = Player(constants.WINDOW_LENGTH/2, (constants.WINDOW_HEIGHT*4)/5, gameDisplay)
+    player = Player(100, (constants.WINDOW_HEIGHT*4)/5, gameDisplay)      
+    # player = Player(constants.WINDOW_LENGTH/2, (constants.WINDOW_HEIGHT*4)/5, gameDisplay)
     bullet = Bullet(constants.WINDOW_LENGTH/2, (constants.WINDOW_HEIGHT*4)/5, gameDisplay)
-
     running = True
+    isShooting = False
+
+    bullet_list = []
 
     while running:
 
@@ -137,18 +128,26 @@ def main():
             if e.type == KEYDOWN:
                 if e.key == K_ESCAPE:
                     running = False
-
-                
+                if e.key == K_SPACE:
+                    isShooting = True
 
             elif e.type == QUIT:
                 running = False
+            
 
         pressed_keys = pygame.key.get_pressed()
 
         gameDisplay.fill((0, 0, 0))
         player.update(pressed_keys)
-        bullet.update(pressed_keys)
 
+        if isShooting:
+            player.shoot(bullet_list)
+        if bullet_list:
+            bullet_list[0].update()
+            
+
+        # print(bullet.x)
+        # print(bullet.y)
         pygame.display.flip()
 
 
